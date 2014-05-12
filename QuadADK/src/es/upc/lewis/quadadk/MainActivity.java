1e12485c18d176d00fa41e88fd409eef5c2a9412
carru <carru400@gmail.com>
2014-05-12 13:30:44 +0200

    Get GPS position

--- QuadADK/src/es/upc/lewis/quadadk/MainActivity.java
+++ QuadADK/src/es/upc/lewis/quadadk/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.support.v4.content.LocalBroadcastManager;
@@ -27,28 +28,32 @@ import com.android.future.usb.UsbManager;
 
 public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
-	
+
 	private PendingIntent mPermissionIntent;
 	private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
 	private boolean mPermissionRequestPending;
-	
+
 	private UsbManager mUsbManager;
 	private UsbAccessory mAccessory;
 	private ParcelFileDescriptor mFileDescriptor;
-	
+
 	// Worker thread for ADK communications
 	private CommunicationsThread comms;
-	
+
 	// Class to communicate with the GroundStation
 	private GroundStationClient groundStation;
-	
+
 	// Camera
 	private SimpleCamera camera;
 	
+	// Location
+	private MyLocation locationProvider;
+	private Location location;
+
 	// Preferences to store socket details
 	SharedPreferences sharedPreferences;
 	SharedPreferences.Editor sharedPreferencesEditor;
-	
+
 	// UI references
 	private TextView adkStatusText;
 	private TextView serverStatusText;
@@ -70,6 +75,8 @@ public class MainActivity extends Activity {
 	private Button connectToServerButton;
 	private EditText ipEditText;
 	private EditText portEditText;
+	private TextView latitude;
+	private TextView longitude;
 	// UI states
 	public static final int CONNECTED = 1;
 	public static final int CONNECTING = 2;
@@ -79,15 +86,17 @@ public class MainActivity extends Activity {
 		mFileDescriptor = mUsbManager.openAccessory(accessory);
 		if (mFileDescriptor != null) {
 			mAccessory = accessory;
-			
-			comms = new CommunicationsThread(this, mFileDescriptor.getFileDescriptor());
+
+			comms = new CommunicationsThread(this,
+					mFileDescriptor.getFileDescriptor());
 			comms.start();
-			
+
 			setADKStatus(CONNECTED);
 			Toast.makeText(this, "Accessory opened", Toast.LENGTH_SHORT).show();
 			Log.i(TAG, "Accessory opened");
 		} else {
-			Toast.makeText(this, "Error opening accessory", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Error opening accessory", Toast.LENGTH_SHORT)
+					.show();
 			Log.e(TAG, "Error opening accessory");
 		}
 	}
@@ -109,16 +118,18 @@ public class MainActivity extends Activity {
 			mAccessory = null;
 
 		}
-		
+
 		setADKStatus(DISCONNECTED);
 	}
-	
+
 	private OnClickListener readSensorButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			if (mAccessory == null) { return; }
-			
-			switch(v.getId()) {
+			if (mAccessory == null) {
+				return;
+			}
+
+			switch (v.getId()) {
 			case R.id.read_sensor_1_button:
 				comms.send(Commands.READ_SENSOR_1);
 				break;
@@ -131,13 +142,15 @@ public class MainActivity extends Activity {
 			}
 		}
 	};
-	
+
 	private OnClickListener setCHButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			if (mAccessory == null) { return; }
-			
-			switch(v.getId()) {
+			if (mAccessory == null) {
+				return;
+			}
+
+			switch (v.getId()) {
 			case R.id.button1:
 				comms.send(Commands.SET_CH1, 1250);
 				break;
@@ -165,14 +178,16 @@ public class MainActivity extends Activity {
 			}
 		}
 	};
-	
+
 	private OnClickListener cameraButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			if (camera.isReady()) { camera.takePicture(); }
+			if (camera.isReady()) {
+				camera.takePicture();
+			}
 		}
 	};
-	
+
 	private OnClickListener connectToServerButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -180,45 +195,49 @@ public class MainActivity extends Activity {
 			int port;
 			try {
 				port = Integer.parseInt(portEditText.getText().toString());
-			} catch(NumberFormatException e) {
-				Toast.makeText(getApplicationContext(), "Invalid port", Toast.LENGTH_SHORT).show();
+			} catch (NumberFormatException e) {
+				Toast.makeText(getApplicationContext(), "Invalid port",
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			
-			groundStation = new GroundStationClient(ip, port, getApplicationContext());
-			
+
+			groundStation = new GroundStationClient(ip, port,
+					getApplicationContext());
+
 			// Save socket details
 			sharedPreferencesEditor.putString("ip", ip);
 			sharedPreferencesEditor.putInt("port", port);
 			sharedPreferencesEditor.apply();
 		}
 	};
-	
+
 	private void connect() {
 		UsbAccessory[] accessories = mUsbManager.getAccessoryList();
 		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
-		
+
 		if (accessory != null) {
 			if (mUsbManager.hasPermission(accessory)) {
 				openAccessory(accessory);
 			} else {
 				synchronized (usbReceiver) {
 					if (!mPermissionRequestPending) {
-						mUsbManager.requestPermission(accessory, mPermissionIntent);
+						mUsbManager.requestPermission(accessory,
+								mPermissionIntent);
 						mPermissionRequestPending = true;
 					}
 				}
 			}
 		} else {
-			Toast.makeText(this, "Error connecting to Arduino", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Error connecting to Arduino",
+					Toast.LENGTH_SHORT).show();
 			Log.e(TAG, "Error connecting to Arduino");
 		}
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main_activity);
 		getUiReferences();
 		readSensor1Button.setOnClickListener(readSensorButtonListener);
@@ -234,39 +253,48 @@ public class MainActivity extends Activity {
 		ch4bButton.setOnClickListener(setCHButtonListener);
 		CameraButton.setOnClickListener(cameraButtonListener);
 		connectToServerButton.setOnClickListener(connectToServerButtonListener);
-		
+
 		setADKStatus(DISCONNECTED);
 		setServerStatus(DISCONNECTED);
-		
+
 		mUsbManager = UsbManager.getInstance(this);
-		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+				ACTION_USB_PERMISSION), 0);
 		registerReceivers();
-		
-		camera = new SimpleCamera(this, (FrameLayout) findViewById(R.id.camera_preview));
+
+		camera = new SimpleCamera(this,
+				(FrameLayout) findViewById(R.id.camera_preview));
+
+		locationProvider = new MyLocation(this);
 		
 		// Get last socket details
 		sharedPreferences = getPreferences(Context.MODE_PRIVATE);
 		sharedPreferencesEditor = sharedPreferences.edit();
 		ipEditText.setText(sharedPreferences.getString("ip", ""));
-		portEditText.setText(Integer.toString(sharedPreferences.getInt("port", 9090)));
+		portEditText.setText(Integer.toString(sharedPreferences.getInt("port",
+				9090)));
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
-		if (mAccessory == null) { connect(); }
+
+		if (mAccessory == null) {
+			connect();
+		}
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		closeAccessory();
 		unregisterReceivers();
-		
+
 		if (camera != null) { camera.close(); }
+		
+		if (locationProvider != null) { locationProvider.stop(); locationProvider = null; }
 	}
-	
+
 	private void getUiReferences() {
 		adkStatusText = (TextView) findViewById(R.id.adk_status);
 		serverStatusText = (TextView) findViewById(R.id.server_status);
@@ -288,150 +316,194 @@ public class MainActivity extends Activity {
 		connectToServerButton = (Button) findViewById(R.id.server_connect_button);
 		ipEditText = (EditText) findViewById(R.id.server_ip);
 		portEditText = (EditText) findViewById(R.id.server_port);
+		latitude = (TextView) findViewById(R.id.latitudeText);
+		longitude = (TextView) findViewById(R.id.longitudeText);
 	}
-	
+
 	private void setADKStatus(int type) {
-    	switch (type) {
-    		case CONNECTED:
-    			adkStatusText.setText("Connected");
-    			adkStatusText.setTextColor(Color.GREEN);
-    			break;
-    		case DISCONNECTED:
-    			adkStatusText.setText("Disconnected");
-    			adkStatusText.setTextColor(Color.RED);
-    			break;
-    	}
-    }
-	
+		switch (type) {
+		case CONNECTED:
+			adkStatusText.setText("Connected");
+			adkStatusText.setTextColor(Color.GREEN);
+			break;
+		case DISCONNECTED:
+			adkStatusText.setText("Disconnected");
+			adkStatusText.setTextColor(Color.RED);
+			break;
+		}
+	}
+
 	private void setServerStatus(int type) {
-    	switch (type) {
-    		case CONNECTED:
-    			serverStatusText.setText("Connected");
-    			serverStatusText.setTextColor(Color.GREEN);
-    			break;
-    		case CONNECTING:
-    			serverStatusText.setText("Connecting");
-    			serverStatusText.setTextColor(Color.YELLOW);
-    			break;
-    		case DISCONNECTED:
-    			serverStatusText.setText("Disconnected");
-    			serverStatusText.setTextColor(Color.RED);
-    			break;
-    	}
-    }
-	
+		switch (type) {
+		case CONNECTED:
+			serverStatusText.setText("Connected");
+			serverStatusText.setTextColor(Color.GREEN);
+			break;
+		case CONNECTING:
+			serverStatusText.setText("Connecting");
+			serverStatusText.setTextColor(Color.YELLOW);
+			break;
+		case DISCONNECTED:
+			serverStatusText.setText("Disconnected");
+			serverStatusText.setTextColor(Color.RED);
+			break;
+		}
+	}
+
 	private void mission() {
 		Log.i(TAG, "Starting mission");
 		Toast.makeText(this, "Starting mission", Toast.LENGTH_SHORT).show();
 	}
-	
+
 	private void sendSensorData(byte sensor, int value) {
 		groundStation.send(sensor, value);
+	}
+
+	private void displayLocation(Location location) {
+		if (location == null) { return; }
+		
+		latitude.setText(Double.toString(location.getLatitude()));
+		longitude.setText(Double.toString(location.getLongitude()));
 	}
 	
 	private void registerReceivers() {
 		// Sensor data receiver
-    	LocalBroadcastManager.getInstance(this).registerReceiver(sensorDataReceiver,sensorDataIntentFilter());
-    	
-    	// GroundStation receiver
-    	LocalBroadcastManager.getInstance(this).registerReceiver(groundStationClientReceiver,groundStationClientIntentFilter());
-    	
-    	// USB events
-    	IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+		LocalBroadcastManager.getInstance(this).registerReceiver(
+				sensorDataReceiver, sensorDataIntentFilter());
+
+		// GroundStation receiver
+		LocalBroadcastManager.getInstance(this).registerReceiver(
+				groundStationClientReceiver, groundStationClientIntentFilter());
+		
+		// Location receiver
+		LocalBroadcastManager.getInstance(this).registerReceiver(
+				locationReceiver, locationIntentFilter());
+
+		// USB events
+		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
 		registerReceiver(usbReceiver, filter);
-    }
-    
-    private void unregisterReceivers() {
-    	// Sensor data receiver
-    	LocalBroadcastManager.getInstance(this).unregisterReceiver(sensorDataReceiver);
-    	
-    	// USB events
-    	unregisterReceiver(usbReceiver);
-    }
-    
-    // Receiver for Arduino related intents
-    private BroadcastReceiver sensorDataReceiver = new BroadcastReceiver() {
-    	  @Override
-    	  public void onReceive(Context context, Intent intent) {
-    		  String action = intent.getAction();
-    		  int value = intent.getIntExtra(CommunicationsThread.VALUE, 0);
-    		  
-    		  if (action.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_1)) {
-    			  sensor1Text.setText(Integer.toString(value));
-    			  sendSensorData(Commands.SENSOR_1, value);
-    		  }
-    		  else if (action.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_2)) {
-    			  sensor2Text.setText(Integer.toString(value));
-    			  sendSensorData(Commands.SENSOR_2, value);
-    		  }
-    		  else if (action.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_3)) {
-    			  sensor3Text.setText(Integer.toString(value));
-    			  sendSensorData(Commands.SENSOR_3, value);
-    		  }
-    	  }
-    };
-    
-    private static IntentFilter sensorDataIntentFilter() {
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_1);
-        intentFilter.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_2);
-        intentFilter.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_3);
-        return intentFilter;
-    }
-    
-    // Receiver for GroundStation related intents
-    private BroadcastReceiver groundStationClientReceiver = new BroadcastReceiver() {
-  	  @Override
-  	  public void onReceive(Context context, Intent intent) {
-  		  String action = intent.getAction();
-  		  
-  		  if (action.equals(GroundStationClient.CONNECTED)) {
-  			  setServerStatus(MainActivity.CONNECTED);
-  		  }
-  		  else if (action.equals(GroundStationClient.CONNECTING)) {
-  			setServerStatus(MainActivity.CONNECTING);
-  		  }
-  		  else if (action.equals(GroundStationClient.DISCONNECTED)) {
-  			setServerStatus(MainActivity.DISCONNECTED);
-  		  }
-  		  else if (action.equals(GroundStationClient.CANT_RESOLVE_HOST)) {
-  			Toast.makeText(getApplicationContext(), "Can't resolve host", Toast.LENGTH_SHORT).show();
-  		  }
-  		  else if (action.equals(GroundStationClient.START_MISSION)) {
-  			mission();
-  		  }
-  	  }
-  };
-  
-  private static IntentFilter groundStationClientIntentFilter() {
-      final IntentFilter intentFilter = new IntentFilter();
-      intentFilter.addAction(GroundStationClient.CONNECTED);
-      intentFilter.addAction(GroundStationClient.CONNECTING);
-      intentFilter.addAction(GroundStationClient.DISCONNECTED);
-      intentFilter.addAction(GroundStationClient.CANT_RESOLVE_HOST);
-      intentFilter.addAction(GroundStationClient.START_MISSION);
-      return intentFilter;
-  }
-    
-    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+	}
+
+	private void unregisterReceivers() {
+		// Sensor data receiver
+		LocalBroadcastManager.getInstance(this).unregisterReceiver(
+				sensorDataReceiver);
+
+		// GroundStation receiver
+		LocalBroadcastManager.getInstance(this).unregisterReceiver(
+				groundStationClientReceiver);
+
+		// Location receiver
+		LocalBroadcastManager.getInstance(this).unregisterReceiver(
+				locationReceiver);
+
+		// USB events
+		unregisterReceiver(usbReceiver);
+	}
+
+	// Receiver for location updates
+		private BroadcastReceiver locationReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				location = locationProvider.getLastLocation();
+				
+				displayLocation(location);
+			}
+		};
+
+		private static IntentFilter locationIntentFilter() {
+			final IntentFilter intentFilter = new IntentFilter();
+			intentFilter.addAction(MyLocation.GPS_UPDATE);
+			return intentFilter;
+		}
+	
+	// Receiver for Arduino application
+	private BroadcastReceiver sensorDataReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
-			
+			int value = intent.getIntExtra(CommunicationsThread.VALUE, 0);
+
+			if (action
+					.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_1)) {
+				sensor1Text.setText(Integer.toString(value));
+				sendSensorData(Commands.SENSOR_1, value);
+			} else if (action
+					.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_2)) {
+				sensor2Text.setText(Integer.toString(value));
+				sendSensorData(Commands.SENSOR_2, value);
+			} else if (action
+					.equals(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_3)) {
+				sensor3Text.setText(Integer.toString(value));
+				sendSensorData(Commands.SENSOR_3, value);
+			}
+		}
+	};
+
+	private static IntentFilter sensorDataIntentFilter() {
+		final IntentFilter intentFilter = new IntentFilter();
+		intentFilter
+				.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_1);
+		intentFilter
+				.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_2);
+		intentFilter
+				.addAction(CommunicationsThread.ACTION_DATA_AVAILABLE_SENSOR_3);
+		return intentFilter;
+	}
+
+	// Receiver for GroundStation related intents
+	private BroadcastReceiver groundStationClientReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+
+			if (action.equals(GroundStationClient.CONNECTED)) {
+				setServerStatus(MainActivity.CONNECTED);
+			} else if (action.equals(GroundStationClient.CONNECTING)) {
+				setServerStatus(MainActivity.CONNECTING);
+			} else if (action.equals(GroundStationClient.DISCONNECTED)) {
+				setServerStatus(MainActivity.DISCONNECTED);
+			} else if (action.equals(GroundStationClient.CANT_RESOLVE_HOST)) {
+				Toast.makeText(getApplicationContext(), "Can't resolve host",
+						Toast.LENGTH_SHORT).show();
+			} else if (action.equals(GroundStationClient.START_MISSION)) {
+				mission();
+			}
+		}
+	};
+
+	private static IntentFilter groundStationClientIntentFilter() {
+		final IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(GroundStationClient.CONNECTED);
+		intentFilter.addAction(GroundStationClient.CONNECTING);
+		intentFilter.addAction(GroundStationClient.DISCONNECTED);
+		intentFilter.addAction(GroundStationClient.CANT_RESOLVE_HOST);
+		intentFilter.addAction(GroundStationClient.START_MISSION);
+		return intentFilter;
+	}
+
+	// Receiver for Arduino USB communication
+	private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+
 			// Granted USB permission
 			if (ACTION_USB_PERMISSION.equals(action)) {
 				synchronized (this) {
 					UsbAccessory accessory = UsbManager.getAccessory(intent);
-					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+					if (intent.getBooleanExtra(
+							UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
 						openAccessory(accessory);
 					} else {
-						Log.e(TAG, "Permission denied for accessory " + accessory);
+						Log.e(TAG, "Permission denied for accessory "
+								+ accessory);
 					}
 					mPermissionRequestPending = false;
 				}
-				
-			// USB disconnected
+
+				// USB disconnected
 			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
 				UsbAccessory accessory = UsbManager.getAccessory(intent);
 				if (accessory != null && accessory.equals(mAccessory)) {
---
+++ QuadADK/src/es/upc/lewis/quadadk/MyLocation.java
@@ -0,0 +1,63 @@
+package es.upc.lewis.quadadk;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+
+public class MyLocation implements LocationListener {
+	private long MIN_TIME_BETWEEN_UPDATES = 100;   // Milliseconds, not accurate
+	private long MIN_DISTANCE_BETWEEN_UPDATES = 0; // Meters
+
+	private Context context;
+	
+	public static final String GPS_UPDATE = "g";
+	Intent intent;
+	
+	private LocationManager locationManager;
+	private Location lastLocation = null;
+
+	public MyLocation(Context context) {
+		this.context = context;
+		
+		locationManager = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_BETWEEN_UPDATES, this);
+	}
+
+	public Location getLastLocation() {
+		return lastLocation;
+	}
+	
+	public void stop() {
+		locationManager.removeUpdates(this);
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		lastLocation = location;
+		
+		// Notify there's an update
+		intent = new Intent(GPS_UPDATE);
+		LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+
+	}
+}
